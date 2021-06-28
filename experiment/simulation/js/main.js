89b57ef0178000ed6a54d9dccbba8596c7325326
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-06-29 00:25:30 +0530

    Main animation almost done.

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -38,6 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
 			if(step === 3)
 			{
 				objs['soil'].color = "#b86d29";
+				keys = keys.filter(function(val, index) {
+					return val != "water";
+				});
 			}
 
 			else if(step === 4)
@@ -153,21 +156,48 @@ document.addEventListener('DOMContentLoaded', function() {
 	};
 
 	class soilPat {
-		constructor(angle, radius, x, y) {
+		constructor(height, width, angle, x, y) {
+			this.width = width; 
+			this.height = height;
 			this.angle = angle;
-			this.radius = radius;
 			this.pos = [x, y];
+			this.grooveDims = [0, 0];
 		};
 
 		draw(ctx) {
-			ctx.beginPath();
 			ctx.fillStyle = "#b86d29";
-			ctx.lineWidth = lineWidth;
+			ctx.lineWidth = 0.001;
+			const e1 = [this.pos[0] + this.width, this.pos[1] + this.height], e2 = [...this.pos], gradX = (e1[0] - e2[0]) / 2, gradY = (e1[1] - e2[1]) / 4;
+			const inter = [((10 - this.grooveDims[0]) * e1[0] + this.grooveDims[0] * e2[0]) / 10, ((10 - this.grooveDims[0]) * e1[1] + this.grooveDims[0] * e2[1]) / 10];
+
 			ctx.beginPath();
-			ctx.ellipse(this.pos[0], this.pos[1], this.radius[0], this.radius[1], this.angle, 0, 2 * Math.PI);
+			ctx.moveTo(e2[0], e2[1]);
+			ctx.bezierCurveTo(e2[0] - 3 * gradX, e2[1] + gradY, e1[0] - 2.5 * gradX, e1[1] + gradY, e1[0], e1[1]);
+			ctx.lineTo(e1[0] - this.grooveDims[1] / 2, e1[1]);
+			ctx.lineTo(inter[0] - this.grooveDims[1] / 2, inter[1]);
+			ctx.lineTo(inter[0] + this.grooveDims[1] / 2, inter[1]);
+			ctx.lineTo(e2[0], e2[1]);
+
+			ctx.moveTo(e1[0], e1[1]);
+			ctx.bezierCurveTo(e1[0] + 3.5 * gradX, e1[1] - gradY, e2[0] + 2 * gradX, e2[1] - gradY, e2[0], e2[1]);
+			ctx.lineTo(inter[0], inter[1]);
+			ctx.lineTo(inter[0] + this.grooveDims[1] / 2, inter[1]);
+			ctx.lineTo(e1[0] + this.grooveDims[1] / 2, e1[1]);
+
 			ctx.closePath();
 			ctx.fill();
 			ctx.stroke();
+		};
+
+		groove(change) {
+			this.grooveDims[0] += change;
+
+			if(this.grooveDims[0] >= 10)
+			{
+				return 1;
+			}
+
+			return 0;
 		};
 	};
 
@@ -180,7 +210,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		draw(ctx) {
 			ctx.fillStyle = "#1ca3ec";
-			ctx.lineWidth = 3;
+			ctx.lineWidth = lineWidth;
 
 			ctx.beginPath();
 			ctx.rect(this.pos[0], this.pos[1], this.width, this.height);
@@ -210,66 +240,86 @@ document.addEventListener('DOMContentLoaded', function() {
 		};
 	};
 
-	class oven {
-		constructor(height, width, x, y) {
+	class casagrandeSide {
+		constructor(height, width, angle, x, y) {
 			this.height = height;
-			this.width = width;
+			this.width = width; 
+			this.angle = angle; 
 			this.pos = [x, y];
+			this.knobAngle = angle;
 		};
 
 		draw(ctx) {
-			// Outline
-			ctx.fillStyle = "white";
-			ctx.lineWidth = 3;
-			ctx.beginPath();
-			ctx.rect(this.pos[0], this.pos[1], this.width, this.height);
-			ctx.closePath();
-			ctx.fill();
-			ctx.stroke();
-
-			// Lower division(red part)
-			const divide = 0.80;
-			ctx.fillStyle = "red";
+			// Lower horizontal part
+			const vertDivide = 0.60;
+			ctx.fillStyle = "#654321";
 			ctx.lineWidth = lineWidth;
 			ctx.beginPath();
-			ctx.rect(this.pos[0], this.pos[1] + divide * this.height, this.width, (1 - divide) * this.height);
+			ctx.rect(this.pos[0], this.pos[1] + vertDivide * this.height, this.width, (1 - vertDivide) * this.height);
 			ctx.closePath();
 			ctx.fill();
 			ctx.stroke();
 
-			// Main segment(pink part)
-			const gap = 0.05;
-			ctx.fillStyle = "pink";
-			ctx.lineWidth = lineWidth;
+			// Right vertical part
+			const horizDivide = 0.80;
+			ctx.fillStyle = "#654321";
 			ctx.beginPath();
-			ctx.rect(this.pos[0] + gap * this.width, this.pos[1] + gap * this.height, (1 - 2 * gap) * this.width, (divide - gap) * this.height);
+			ctx.rect(this.pos[0] + horizDivide * this.width, this.pos[1], (1 - horizDivide) * this.width, vertDivide * this.height);
 			ctx.closePath();
 			ctx.fill();
 			ctx.stroke();
 
-			// Horizontal rectangle at bottom
-			const margin = [0.30, 0.05];
-			ctx.fillStyle = "white";
-			ctx.lineWidth = lineWidth;
+			// Main cup part
+			ctx.fillStyle = "yellow";
+			const gap = 0.2, e1 = [this.pos[0] + horizDivide * this.width, this.pos[1] + gap * this.height], e2 = [this.pos[0], this.pos[1] + gap * this.height], gradX = (e1[0] - e2[0]) / -4, gradY = 60;
+			const center = [e1[0] + (1 - horizDivide) * this.width / 2, e1[1]];
+
+			ctx.translate(e1[0], e1[1]);
+			ctx.rotate(this.angle);
+			e2[0] -= e1[0];
+			e2[1] -= e1[1];
+			e1[0] = 0;
+			e1[1] = 0;
+
 			ctx.beginPath();
-			ctx.rect(this.pos[0] + margin[0] * this.width, this.pos[1] + (margin[1] + divide) * this.height, (1 - 2 * margin[0]) * this.width, (1 - divide - 2 * margin[1]) * this.height);
+			ctx.moveTo(e2[0], e2[1]);
+			ctx.lineTo(e1[0], e1[1]);
+			ctx.bezierCurveTo(e1[0], e1[1] + 0.7 * gradY, e2[0], e2[1] + gradY, e2[0], e2[1]);
 			ctx.closePath();
 			ctx.fill();
 			ctx.stroke();
+			ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-			ctx.font = "30px Arial";
-			ctx.fillStyle = "black";
-			ctx.fillText("Oven", this.pos[0] + margin[0] * this.width + 10, this.pos[1] + (margin[1] + divide) * this.height + 25);
+			// Knob
+			ctx.fillStyle = "#A9A9A9";
+			ctx.lineWidth = 0.001;
+			let radius = 0, angle = this.knobAngle; 
 
-			// Small button at bottom
-			const buttonGapX = 0.10;
-			ctx.fillStyle = "white";
-			ctx.lineWidth = lineWidth;
 			ctx.beginPath();
-			ctx.rect(this.pos[0] + buttonGapX * this.width, this.pos[1] + (margin[1] + divide) * this.height, (margin[0] - 2 * buttonGapX) * this.width, (1 - divide - 2 * margin[1]) * this.height);
+			ctx.moveTo(center[0], center[1]);
+			for (let n = 0; n < 75; n++) {
+				radius += 0.25;
+				angle += Math.PI * 2 / 50;
+				const x = center[0] + radius * Math.cos(angle), y = center[1] + radius * Math.sin(angle);
+				ctx.lineTo(x, y);
+			}
+			ctx.lineTo(center[0], center[1]);
+
 			ctx.closePath();
 			ctx.fill();
 			ctx.stroke();
+		};
+
+		rotate(rotAng) {
+			this.angle += rotAng;
+			this.knobAngle += math.abs(rotAng * 16); 
+
+			if(this.angle < -Math.PI / 16 || this.angle >= 0)
+			{
+				return -1;
+			}
+
+			return 1;
 		};
 	};
 
@@ -286,10 +336,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		};
 		keys = [];
 
-		enabled = [["container"], ["container", "soil"], ["container", "soil", "water"], ["container", "soil"], ["container", "soil", "casagrande"], ["soil", "casagrande"], ["container", "soil", "oven"], ["weight", "container", "soil"], []];
+		enabled = [["container"], ["container", "soil"], ["container", "soil", "water"], ["container", "soil"], ["container", "soil", "casagrande"], ["soil", "casagrande"], ["soil", "casagrande"], ["soil", "casagrande"], ["weight", "container", "soil"], []];
 		step = 0;
 		translate = [0, 0];
 		lim = [-1, -1];
+		revolts = 0;
+		rots = 0;
 	};
 
 	function restart() 
@@ -354,30 +406,37 @@ document.addEventListener('DOMContentLoaded', function() {
 					hover = true;
 					if(flag)
 					{
-						const center = [objs['soil'].pos[0] + objs['soil'].width / 2, objs['soil'].pos[1] + objs['soil'].height / 2], radius = [30, 45]; 
-						objs['soil'] = new soilPat(75 * Math.PI / 180, radius, center[0], center[1]);
+						const center = [objs['soil'].pos[0] + objs['soil'].width / 2, objs['soil'].pos[1] + objs['soil'].height / 2];
+						objs['soil'] = new soilPat(-55, 40, 60 * Math.PI / 180, center[0], center[1]);
 					}
 
 					translate[0] = 5;
 					translate[1] = -5;
-					lim[0] = 625;
-					lim[1] = 275;
+					lim[0] = 605;
+					lim[1] = 305;
 				}
 
-				else if(step === 6 && val === "container")
+				else if(step === 6 && val === "casagrande")
 				{
 					hover = true;
-					translate[0] = 5;
-					translate[1] = 5;
-					lim[0] = 560;
-					lim[1] = 150;
+					translate[0] = 0.5;
+					if(flag)
+					{
+						objs['soil'].grooveDims[1] = 10;
+					}
 				}
 
-				else if(step === 7 && val === "oven" && canvasPos[0] >= objs[val].pos[0] - errMargin && canvasPos[0] <= objs[val].pos[0] + objs[val].width + errMargin && canvasPos[1] >= objs[val].pos[1] + objs[val].height * 0.8 - errMargin && canvasPos[1] <= objs[val].pos[1] + objs[val].height + errMargin)
+				else if(step === 7 && val === "casagrande")
 				{
 					hover = true;
-					translate[1] = 1;
-					lim[1] = 210;
+					if(flag)
+					{
+						objs['casagrande'] = new casagrandeSide(150, 200, 0, 550, 230);
+						keys = keys.filter(function(val, index) {
+							return val != "soil";
+						});
+						translate[0] = -Math.PI / (16 * 50);
+					}
 				}
 
 				else if(step === 8 && val === "container")
@@ -427,7 +486,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		"Click the restart button to perform the experiment again.",
 	];
 
-	let step, translate, lim, objs, keys, enabled, small;
+	let step, translate, lim, objs, keys, enabled, small, revolts, rots;
 	init();
 
 	const tableData = [
@@ -510,22 +569,42 @@ document.addEventListener('DOMContentLoaded', function() {
 		if(translate[0] != 0 || translate[1] != 0)
 		{
 			let temp = step;
-			const soilMoves = [5, 6, 7, 8], waterMoves = [3];
+			const soilMoves = [5], waterMoves = [3];
 
-			if(soilMoves.includes(step))
+			if(step === 6)
+			{
+				temp += objs['soil'].groove(translate[0]);
+				if(temp != step)
+				{
+					translate[0] = 0;
+				}
+			}
+
+			else if(step === 7)
+			{
+				translate[0] *= objs['casagrande'].rotate(translate[0]);
+				rots = (rots + 1) % (2 * 50);
+				if(rots === 0)
+				{
+					revolts += 1;
+					if(revolts >= 10)
+					{
+						translate[0] = 0;
+						revolts = 0;
+						temp += 1;
+					}
+				}
+			}
+
+			else if(soilMoves.includes(step))
 			{
 				updatePos(objs['soil'], translate);
 				temp = limCheck(objs['soil'], translate, lim, step);
 			}
 
-			if(waterMoves.includes(step))
+			else if(waterMoves.includes(step))
 			{
-				updatePos(objs['water'], translate);
-				if(step === 3)
-				{
-					objs['water'].mixing(translate[1]);
-				}
-
+				objs['water'].mixing(translate[1]);
 				temp = limCheck(objs['water'], translate, lim, step);
 			}
 
